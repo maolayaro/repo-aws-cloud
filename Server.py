a2ae34f45ed338027a612ea2f2be520cd2ddbcb9
EC2 Default User <ec2-user@ip-172-31-20-168.us-west-1.compute.internal>
2024-09-24 02:27:10 +0000

    version1

--- Server.py
+++ Server.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
-app = Flask (__name__)
+from flask import Flask, render_template, request
+from database.db import *
+app = Flask(__name__)
 
 @app.route('/Tarea')
 def Tarea():
     print("HOLA")
     return render_template("Tarea.html")
 
+@app.route('/Registro', methods=['POST'])
+def Registro():
+    data_registro = request.form
+    Nombre = data_registro["Nombre"]
+    Cargo = data_registro["Cargo"]
+    ID = data_registro["ID"]
+    Actividad = data_registro["Actividad"]
+    Descripcion = data_registro["Descripcion"]
+    Fecha = data_registro["Fecha"]
+    Tiempo = data_registro["Tiempo"]
+    print(data_registro)
+    add_registro(Nombre, Cargo, ID, Actividad, Descripcion, Fecha, Tiempo)
+    return "User added"
 
 if __name__ == "__main__":
     ip = "172.31.20.168"
-    port = "80"
-    app.run(ip, port)
-
-rm -rf .git
+    port = 80
+    app.run(host=ip, port=port)  # Especificar 'host' y 'port'
--- database/db.py
+++ database/db.py
@@ -4,22 +4,39 @@ import pymysql
 endpoint = 'ins-bd.cl0o4cyeqk1l.us-west-1.rds.amazonaws.com'
 user = 'admin'
 password = 'Maor.1033734720'
+database = 'db_registro'
 
 def connectionSQL():
     connection = None
     try:
         # Establecer la conexión
-        pymysql.connect(
+        connection = pymysql.connect(  # Aquí se debe usar '='
             host=endpoint,
             user=user,
-            password=password
+            password=password,
+            database=database
         )
         print("Conexión exitosa a la base de datos")
+        return connection
     except pymysql.MySQLError as err:
         print("Error al conectar a la base de datos:", err)
+        return None
     finally:
         # Cerrar la conexión si está abierta
         if connection:
             connection.close()
 
-connectionSQL()
+def add_registro(Nombre, Cargo, ID, Actividad, Descripcion, Fecha, Tiempo):
+    try:
+        instruction_sql = "INSERT INTO registro (Nombre, Cargo, ID, Actividad, Descripcion, Fecha, Tiempo) VALUES('Maira', 'ing', 12234, 'fw', 'regla', '2024-09-24',14)"
+        connection = connectionSQL()
+        cursor = connection.cursor() 
+        cursor.execute(instruction_sql)
+        connection.commit()
+        #obj_connection.commit()
+        #print("Se registro la actividad con exito")
+    except Exception as err:
+        print("Error al conectar a la base de datos:", err)
+
+
+
